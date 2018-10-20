93c3263b2cd69c7b9214a55cd321c8dcf40de490
susmita000 <susmitapanigrahi6@gmail.com>
2018-10-20 15:38:26 +0530

    added some lines

---
+++ src/main/java/com/javatechie/git_demo_test/App.java
@@ -0,0 +1,13 @@
+package com.javatechie.git_demo_test;
+
+
+public class App 
+{
+    public static void main( String[] args ) {
+    	int i=10;
+    	int j=20;
+    	int sum;
+    	sum=i+j;
+    	System.out.println(sum );
+    }
+}
